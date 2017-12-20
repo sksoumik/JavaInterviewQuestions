3b4b533bbd8b879b19cd5c12cce5b3dbccc4563f
sksoumik <sadmanks@gmail.com>
2017-12-21 03:00:23 +0600

    Java Interview Questions

---
+++ src/arrayListJava/ReverseArrayList.java
@@ -0,0 +1,28 @@
+package arrayListJava;
+// How to reverse ArrayList in Java with Example
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class ReverseArrayList {
+
+    /* We can reverse ArrayList in Java by using the reverse() method of java.util.Collections class */
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList  = new ArrayList<Integer>();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(4);
+        arrayList.add(5);
+        //Also works fine for String as well
+        System.out.println("Before reversing: "+arrayList);
+
+        Collections.reverse(arrayList);
+
+        System.out.println("After reversing: "+arrayList);
+
+
+    }
+
+}
